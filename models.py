4872427868fc4607f50fe30ee69f549f02c5bca9
Nayan Shashidhar <nayan.shashidhar123@gmail.com>
2020-07-01 20:07:48 -0500

    fixed syntax errors in models.py

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLalchemy
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING",) #need to change it
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLalchemy(app)
+db = SQLAlchemy(app)
 
 class Restaurants(db.Model):
-    __tablename__ = 'locations'
+    __tablename__ = 'restaurant'
     id = db.Column(db.Integer, primary_key=True)
     google_api_id = db.Column(db.String, unique=True, nullable=False)
     type = db.Column(db.String, nullable=False)
@@ -17,14 +17,14 @@ class Restaurants(db.Model):
     city = db.Column(db.String, nullable=False)
     zipcode = db.Column(db.String, nullable=False)
     open_status = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Double, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
     price_level = db.Column(db.Integer, nullable=False)
     website = db.Column(db.String, nullable=False)
     img_url = db.Column(db.String, nullable=False)
     business_status = db.Column(db.String, nullable=False)
 
 class Grocery(db.Model):
-    __tablename__ = 'locations'
+    __tablename__ = 'grocery'
     id = db.Column(db.Integer, primary_key=True)
     google_api_id = db.Column(db.String, unique=True, nullable=False)
     type = db.Column(db.String, nullable=False)
@@ -33,14 +33,14 @@ class Grocery(db.Model):
     city = db.Column(db.String, nullable=False)
     zipcode = db.Column(db.String, nullable=False)
     open_status = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Double, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
     price_level = db.Column(db.Integer, nullable=False)
     website = db.Column(db.String, nullable=False)
     img_url = db.Column(db.String, nullable=False)
     business_status = db.Column(db.String, nullable=False)
 
 class Healthcare(db.Model):
-    __tablename__ = 'locations'
+    __tablename__ = 'healthcare'
     id = db.Column(db.Integer, primary_key=True)
     google_api_id = db.Column(db.String, unique=True, nullable=False)
     type = db.Column(db.String, nullable=False)
@@ -49,7 +49,7 @@ class Healthcare(db.Model):
     city = db.Column(db.String, nullable=False)
     zipcode = db.Column(db.String, nullable=False)
     open_status = db.Column(db.String, nullable=False)
-    rating = db.Column(db.Double, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
     price_level = db.Column(db.Integer, nullable=False)
     website = db.Column(db.String, nullable=False)
     img_url = db.Column(db.String, nullable=False)
@@ -59,46 +59,3 @@ class Healthcare(db.Model):
 if __name__ == "__main__":
     db.drop_all()
     db.create_all()
-
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     """
-#        home page, can be redirected to grocery, restaurants, healthcare page, and about page
-#     """
-#     return render_template('index.html')
-
-# @app.route('/grocery', methods=['GET', 'POST'])
-# def grocery():
-#     """
-#         grocery page, contains open/close status and general info for each grocery stores listed
-#     """
-#     return render_template('grocery.html')
-
-# @app.route('/restaurants', methods=['GET', 'POST'])
-# def restaurants():
-#     """
-#         restaurants page, contains open/close status and general info for each restaurants listed
-#     """
-#     return render_template('restaurants.html')
-
-# @app.route('/healthcare', methods=['GET', 'POST'])
-# def healthcare():
-#     """
-#         healthcare page, contains open/close status and general info for each healthcare listed
-#     """
-#     return render_template('healthcare.html')
-
-# @app.route('/about', methods=['GET', 'POST'])
-# def about():
-#     """
-#         about page, contains introductions of each team memeber, data source used, and all the required information
-#     """
-#     return render_template('about.html')
-
-# if __name__ == "__main__":
-#     app.debug = True
-#     app.run()
