384e3605000f4cd61c8aeccbdff5a1d41b29768b
hmallidi <mallidi.harinadha@gmail.com>
2020-07-02 18:48:18 -0500

    fixed bugs in request and parsed addresses to get 'city' and 'zip_code' fields

--- request.py
+++ request.py
@@ -1,6 +1,19 @@
 import googlemaps
 import json
 import time
+import usaddress
+
+
+def getQuery(city, name):
+    name = name.replace(" ", "+")
+    city = city.replace(" ", "+")
+
+    if name == "":
+        query = city
+    else:
+        query = name + "+in+" + city
+
+    return query
 
 
 def reformatLocation(location):
@@ -14,28 +27,42 @@ def reformatLocation(location):
         location["weekday_text"] = weekday_text
         del location["opening_hours"]
 
+    try:
+        parsed_address = usaddress.tag(location["formatted_address"])[0]
+
+        if 'PlaceName' in parsed_address:
+            location['city'] = parsed_address['PlaceName']
+        if 'ZipCode' in parsed_address:
+            location['zip_code'] = parsed_address['ZipCode']
+    except usaddress.RepeatedLabelError:
+        return None
+
     return location
 
 
 def getJSON(place_type, city, name="", min_price=0, max_price=4):
     gmaps = googlemaps.Client(key='AIzaSyDzN3W-S4TxBm3wqslV1_JqfwhLjEsSKI8')
 
-    page_token = ""
+    city = city.strip()
+    name = name.strip()
 
+    page_token = ""
     places_results = list()
+    query = getQuery(city, name)
 
     while True:
-        name = name.replace(" ", "+")
+        places_results_next_page = None
 
-        query = city
-        if name != "":
-            query = name + "+in+" + city
-
-        places_results_next_page = gmaps.places(query=query,
-                                                min_price=min_price,
-                                                max_price=max_price,
-                                                type=place_type,
-                                                page_token=page_token)
+        if place_type == "restaurant":
+            places_results_next_page = gmaps.places(query=query,
+                                                    min_price=min_price,
+                                                    max_price=max_price,
+                                                    type=place_type,
+                                                    page_token=page_token)
+        else:
+            places_results_next_page = gmaps.places(query=query,
+                                                    type=place_type,
+                                                    page_token=page_token)
 
         for location in places_results_next_page["results"]:
 
@@ -54,9 +81,10 @@ def getJSON(place_type, city, name="", min_price=0, max_price=4):
 
             location = reformatLocation(location)
 
-            places_results.append(location)
+            if location is not None:
+                places_results.append(location)
 
-        time.sleep(5)
+        time.sleep(2)
 
         if "next_page_token" in places_results_next_page:
             page_token = places_results_next_page["next_page_token"]
@@ -67,7 +95,7 @@ def getJSON(place_type, city, name="", min_price=0, max_price=4):
 
 
 # RESTAURANTS
-# places_results = getJSON("restaurant", "Amarillo")
+places_results = getJSON("supermarket", "Kansas City")
 
 # HOSPITALS
 # places_results = getJSON("hospital", "Amarillo")
@@ -76,6 +104,26 @@ def getJSON(place_type, city, name="", min_price=0, max_price=4):
 # places_results = getJSON("supermarket", "Amarillo")
 # places_results.extend(getJSON("drugstore", "Amarillo"))
 
-# for location in places_results:
-#     print(location['name'])
+# LOCATIONS DICT
+#   Keys/Info
+#       place_id
+#
+#       name
+#
+#       formatted_address
+#       city
+#       zip-code
+#       location
+#
+#       business_status
+#       icon
+#       opening_hours
+#       price_level
+#       rating
+#
+#       url
+#       website
 
+# for location in places_results:
+#     print(location['city'])
+#     print(location['name'])
