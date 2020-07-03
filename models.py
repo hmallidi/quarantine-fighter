fdc30eb6fea46fb4b028a82b4bdfc38522fbcc76
Nayan Shashidhar <nayan.shashidhar123@gmail.com>
2020-07-03 17:24:35 -0500

    tables set up, except for relational parts, can fix this later though

--- models.py
+++ models.py
@@ -7,24 +7,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING", 'postgres://
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-# class Restaurants(db.Model):
-#     __tablename__ = 'restaurant'
-#     google_api_id = db.Column(db.String, primary_key=True, nullable=False)
-#     type = db.Column(db.String, nullable=False)
-#     name = db.Column(db.String, nullable=False)
-#     address = db.Column(db.String, nullable=False)
-#     city = db.Column(db.String, nullable=False)
-#     zipcode = db.Column(db.String, nullable=False)
-#     open_status = db.Column(db.String, nullable=False)
-#     rating = db.Column(db.Float, nullable=False)
-#     price_level = db.Column(db.Integer, nullable=False)
-#     website = db.Column(db.String, nullable=False)
-#     img_url = db.Column(db.String, nullable=False)
-#     business_status = db.Column(db.String, nullable=False)
-#     phone_number = db.Column(db.String, nullable=False)
-
-class Grocery(db.Model):
-    __tablename__ = 'grocery'
+class Drugstore(db.Model):
+    __tablename__ = 'drugstores'
     google_api_id = db.Column(db.String, primary_key=True, nullable=False)
     type = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
@@ -61,7 +45,6 @@ class City(db.Model):
     population = db.Column(db.Integer, nullable=False)
     state = db.Column(db.String(2), nullable=False)
     zipcode = db.Column(db.String, nullable=False)
-    # covid_cases = db.Column(db.Integer, nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     hospitals = db.Column() #relational to hospital table
