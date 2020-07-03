8bcbe37aa4a242a380a2dd540d7c3c25bfb6747b
Kewen Wang <wankew22@kewens-mbp.lan>
2020-07-03 00:12:54 -0500

    add change for react implemntation

---
+++ react_ver/src/Restaurant.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class Restaurant extends Component {
+  render() {
+    return (
+      
+      <div>
+        <h2>Restaurant</h2>
+        <p>The easiest thing to do is post on
+        our <a href="http://forum.kirupa.com">forums</a>.
+        </p>
+      </div>
+
+
+    );
+  }
+}
+ 
+export default Restaurant;
