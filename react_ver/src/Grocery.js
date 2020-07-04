6888ffccc18e5937bb793a897b43ca9c3a0a9b38
Angela Cao <runningnerdtroll@gmail.com>
2020-07-03 19:37:29 -0500

    started implementing some stuff to react

--- react_ver/src/Grocery.js
+++ react_ver/src/Grocery.js
@@ -1,8 +1,89 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
  
 class Grocery extends Component {
+  function createData(name, state, longitude, latitude, population) {
+    return {name, state, longitude, latitude, population};
+  }
+
+  const rows = [
+    createData('New York', 'NY', 0, 0, 0), 
+    createData('Los Angeles', 'CA', 0, 0, 0),
+    createData('Chicago', 'IL', 0, 0, 0),
+    createData('Houston', 'TX', 0, 0, 0),
+    createData('Austin', 'TX', 0, 0, 0)
+  ];
+
+  function descendingComparator(a, b, orderBy) {
+    if(b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if(b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0; 
+  }
+
+  function getComparator(order, orderBy) {
+    return order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+  }
+  
+  const headCells = [
+
+  ];
+  
   render() {
     return (
+      <HashRouter>
+	        <div>
+	        <h1>COVID	FIGHTER</h1>
+	          <ul className="header">
+	            <li><NavLink exact to="/">Home</NavLink></li>
+	            <li><NavLink exact to="/Grocery">Grocery</NavLink></li>
+	            <li><NavLink exact to="/Restaurant">Restaurant</NavLink></li>
+	            <li><NavLink exact to="/Healthcare">Healthcare</NavLink></li>
+	            <li><NavLink exact to="/About">About</NavLink></li>
+	          </ul>
+	          <div className="content">
+				  <Route exact path="/" component={Home}/>
+				  <Route path="/Grocery" exact component={Grocery}/>
+				  <Route path="/Restaurant" exact component={Restaurant}/>
+				  <Route path="/Healthcare" exact component={Healthcare}/>
+				  <Route path="/About" exact component={About}/>
+				</div>
+	        </div>
+	        
+	    </HashRouter>
       <div>
         <h2>Grocery</h2>
         <p>Mauris sem velit, vehicula eget sodales vitae,
--- react_ver/src/Restaurant.js
+++ react_ver/src/Restaurant.js
@@ -1,9 +1,57 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FilterListIcon from '@material-ui/icons/FilterList';
  
 class Restaurant extends Component {
+  function createData(name, address, phone_number, website, hours) {
+    return {name, address, phone_number, website, hours};
+  }
+
+  const rows = [
+    
+  ];
+
   render() {
     return (
-      
+      <HashRouter>
+	        <div>
+	        <h1>COVID	FIGHTER</h1>
+	          <ul className="header">
+	            <li><NavLink exact to="/">Home</NavLink></li>
+	            <li><NavLink exact to="/Grocery">Grocery</NavLink></li>
+	            <li><NavLink exact to="/Restaurant">Restaurant</NavLink></li>
+	            <li><NavLink exact to="/Healthcare">Healthcare</NavLink></li>
+	            <li><NavLink exact to="/About">About</NavLink></li>
+	          </ul>
+	          <div className="content">
+				  <Route exact path="/" component={Home}/>
+				  <Route path="/Grocery" exact component={Grocery}/>
+				  <Route path="/Restaurant" exact component={Restaurant}/>
+				  <Route path="/Healthcare" exact component={Healthcare}/>
+				  <Route path="/About" exact component={About}/>
+				</div>
+	        </div>
+	        
+	    </HashRouter>
       <div>
         <h2>Restaurant</h2>
         <p>The easiest thing to do is post on
