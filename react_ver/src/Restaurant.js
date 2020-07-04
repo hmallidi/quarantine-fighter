0c4f3f9e582c9e8f7643ab38c0e46c596f3f1940
Angela Cao <runningnerdtroll@gmail.com>
2020-07-04 12:38:53 -0500

    added more react code to the react files

--- react_ver/src/Restaurant.js
+++ react_ver/src/Restaurant.js
@@ -22,13 +22,54 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import FilterListIcon from '@material-ui/icons/FilterList';
  
 class Restaurant extends Component {
+
+  const useStyles = makeStyles({
+	  table: {
+		  minWidth: 650,
+	  },
+  });
+  
   function createData(name, address, phone_number, website, hours) {
     return {name, address, phone_number, website, hours};
   }
 
   const rows = [
-    
+	createData('hospital', 'address', '1234567890', 'website.com', '12AM - 11PM'), 
+	createData('hospital', 'address', '1234567890', 'website.com', '12AM - 11PM')
   ];
+
+  export default function SimpleTable() {
+	const clases = useStyles();
+
+	return(
+		<TableContainer component={Paper}>
+			<Table className={classes.table} aria-label="simple table">
+				<TableHead>
+					<TableRow>
+						<TableCell>Name</TableCell>
+						<TableCell>Address</TableCell>
+						<TableCell>Phone Number</TableCell>
+						<TableCell>Website</TableCell>
+						<TableCell>Hours</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{rows.map((row) => (
+						<TableRow key={row.name}>
+							<Table Cell component="th" scope="row">
+								{row.name}
+							</Table>
+							<TableCell>{row.address}</TableCell>
+							<TableCell>{row.phone_number}</TableCell>
+							<TableCell>{row.website}</TableCell>
+							<TableCell>{row.hours}</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
+  }
 
   render() {
     return (
