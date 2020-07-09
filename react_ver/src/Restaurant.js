df0e2737d09fd43c846486677ce023eda5b45175
Angela Cao <runningnerdtroll@gmail.com>
2020-07-08 22:42:35 -0500

    website is finally working, but need to add the table in

--- react_ver/src/Restaurant.js
+++ react_ver/src/Restaurant.js
@@ -4,25 +4,6 @@ import { HashRouter } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { lighten, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FilterListIcon from '@material-ui/icons/FilterList';
 
 
 class Restaurant extends Component {
@@ -33,7 +14,7 @@ class Restaurant extends Component {
   }
 
   componentDidMount() {
-    fetch("http://hostname:xxxx/yyyy/zzzz")
+    fetch("http://localhost:5000/api/City/1")
       .then(function(response) {
         return response.json();
       })
