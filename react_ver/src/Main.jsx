496aa77116a75edbc0c2dcf6d0ee94ee39c93d94
Nayan Shashidhar <nayan.shashidhar123@gmail.com>
2020-07-11 16:48:19 -0500

    copied over Grocery, Restaurant, and Healthcare.jsx into City, Drugstore, and Hospital.jsx

--- react_ver/src/Main.jsx
+++ react_ver/src/Main.jsx
@@ -5,9 +5,9 @@ import {
   HashRouter
 } from "react-router-dom";
 import Home from "./Home";
-import Grocery from "./Grocery";
-import Restaurant from "./Restaurant";
-import Healthcare from "./Healthcare";
+import City from "./City";
+import Drugstore from "./Drugstore";
+import Hospital from "./Hospital";
 import About from "./About";
  
 class Main extends Component {
@@ -18,16 +18,16 @@ class Main extends Component {
 	        <h1>COVID	FIGHTER</h1>
 	          <ul className="header">
 	            <li><NavLink exact to="/">Home</NavLink></li>
-	            <li><NavLink exact to="/Grocery">Grocery</NavLink></li>
-	            <li><NavLink exact to="/Restaurant">Restaurant</NavLink></li>
-	            <li><NavLink exact to="/Healthcare">Healthcare</NavLink></li>
+	            <li><NavLink exact to="/city">City</NavLink></li>
+	            <li><NavLink exact to="/drugstore">Drugstore</NavLink></li>
+	            <li><NavLink exact to="/hospital">Hospital</NavLink></li>
 	            <li><NavLink exact to="/About">About</NavLink></li>
 	          </ul>
 	          <div className="content">
 				  <Route exact path="/" component={Home}/>
-				  <Route path="/Grocery" exact component={Grocery}/>
-				  <Route path="/Restaurant" exact component={Restaurant}/>
-				  <Route path="/Healthcare" exact component={Healthcare}/>
+				  <Route path="/city" exact component={City}/>
+				  <Route path="/drugstore" exact component={Drugstore}/>
+				  <Route path="/hospital" exact component={Hospital}/>
 				  <Route path="/About" exact component={About}/>
 				</div>
 	        </div>
